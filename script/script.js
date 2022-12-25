4d0d73312c0f35d85b40154995c439cc0f5656ed
Satria <74123282+RatriaSeza@users.noreply.github.com>
2022-12-26 01:04:04 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,6 +1,3 @@
-
-const about_page = document.getElementById("about");
-
 $(document).ready(function () {
     $(window).scroll(function () {
         if ($(window).scrollTop() > 100) {
@@ -15,9 +12,28 @@ $(document).ready(function () {
             $("#logo").html('Portofolio');
         }
         
+        if ($(window).scrollTop() >= 0 && $(window).scrollTop() < 699){
+            $("#link-home").addClass("yellow");
+        } else {
+            $("#link-home").removeClass("yellow");
+        }
 
-        // if (about_page.scrollIntoView()){
-        //     $("#logo").html('Satria Reza Ramadhan');
-        // }
+        if ($(window).scrollTop() > 700 && $(window).scrollTop() < 2900 ) {
+            $("#link-about").addClass("yellow");
+        } else {
+            $("#link-about").removeClass("yellow");
+        }
+
+        if ($(window).scrollTop() > 2900) {
+            $("#link-home").addClass("grey");
+            $("#link-about").addClass("grey");
+            $("#link-contact").addClass("yellow");
+            $("nav p:before").addClass("grey")
+        } else {
+            $("#link-home").removeClass("grey");
+            $("#link-about").removeClass("grey");
+            $("#link-contact").removeClass("yellow");
+            $("nav p:before").removeClass("grey")
+        }
     });
 });
