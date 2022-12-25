ecee2f77a8ce397c78380c1f971369b00a82b5fd
Satria <74123282+RatriaSeza@users.noreply.github.com>
2022-12-25 16:28:12 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,7 +1,23 @@
-$(window).scroll(function () {
-    if ($(window).scrollTop() > 100) {
-        $(".hero").addClass("blur");
-    } else {
-        $(".hero").removeClass("blur");
-    }
+
+const about_page = document.getElementById("about");
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $(".hero").addClass("blur");
+        } else {
+            $(".hero").removeClass("blur");
+        }
+
+        if ($(window).scrollTop() > 700) {
+            $("#logo").html('Satria Reza Ramadhan');
+        } else {
+            $("#logo").html('Portofolio');
+        }
+        
+
+        // if (about_page.scrollIntoView()){
+        //     $("#logo").html('Satria Reza Ramadhan');
+        // }
+    });
 });
