f97e4b50af800f45fbc6ae0c123675f01a8d7b8e
Satria <74123282+RatriaSeza@users.noreply.github.com>
2023-01-12 05:05:00 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,69 +1,75 @@
 // Nav
-function responsive(){
-    var x = document.getElementById("myTopNav");
-    if (x.className === "hero"){
-        x.classList.add("responsive")
-    }else{
-        x.className = "hero";
-    }
+function responsive() {
+	var x = document.getElementById("myTopNav");
+	if (x.className === "hero") {
+		x.classList.add("responsive");
+	} else {
+		x.className = "hero";
+	}
 }
 
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 100) {
-            $(".hero").addClass("blur");
-        } else {
-            $(".hero").removeClass("blur");
-        }
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 100) {
+			$(".hero").addClass("blur");
+		} else {
+			$(".hero").removeClass("blur");
+		}
 
-        if ($(window).scrollTop() > 700) {
-            $("#logo").html('Satria Reza Ramadhan');
-        } else {
-            $("#logo").html('Portofolio');
-        }
-        
-        if ($(window).scrollTop() >= 0 && $(window).scrollTop() < 699){
-            $("#link-home").addClass("yellow");
-        } else {
-            $("#link-home").removeClass("yellow");
-        }
+		if ($(window).scrollTop() > 700) {
+			$("#logo").html('<i class="fa-solid fa-minus"></i>Satria Reza Ramadhan');
+		} else {
+			$("#logo").html('<i class="fa-solid fa-minus"></i>Portofolio');
+		}
 
-        if ($(window).scrollTop() > 700 && $(window).scrollTop() < 2800 ) {
-            $("#link-about").addClass("yellow");
-        } else {
-            $("#link-about").removeClass("yellow");
-        }
-        
-        if ($(window).scrollTop() > 2800) {
-            $("#link-home").addClass("grey");
-            $("#link-about").addClass("grey");
-            $("#link-contact").addClass("yellow");
-            $("p").addClass("green");
-        } else {
-            $("#link-home").removeClass("grey");
-            $("#link-about").removeClass("grey");
-            $("#link-contact").removeClass("yellow");
-            $("p").removeClass("green");
-        }
-    });
+		if ($(window).scrollTop() >= 0 && $(window).scrollTop() < 699) {
+			$("#link-home").addClass("yellow");
+		} else {
+			$("#link-home").removeClass("yellow");
+		}
+
+		if ($(window).scrollTop() > 700 && $(window).scrollTop() < 2800) {
+			$("#link-about").addClass("yellow");
+		} else {
+			$("#link-about").removeClass("yellow");
+		}
+
+		if ($(window).scrollTop() > 2800) {
+			$("#link-home").addClass("grey");
+			$("#link-about").addClass("grey");
+			$("#link-contact").addClass("yellow");
+			$("#logo").addClass("green");
+		} else {
+			$("#link-home").removeClass("grey");
+			$("#link-about").removeClass("grey");
+			$("#link-contact").removeClass("yellow");
+			$("#logo").removeClass("green");
+		}
+	});
 });
 
 // Reveal animation
-function reveal(){
-    var reveals = document.querySelectorAll(".reveal");
+function reveal() {
+	var reveals = document.querySelectorAll(".reveal");
 
-    reveals.forEach((reveal) =>{
-        var windowHeight = window.innerHeight;
-        var elemenTop = reveal.getBoundingClientRect().top;
-        var elementVisible = 100;
+	reveals.forEach((reveal) => {
+		var windowHeight = window.innerHeight;
+		var elemenTop = reveal.getBoundingClientRect().top;
+		var elementVisible = 100;
 
-        if (elemenTop < windowHeight - elementVisible){
-            reveal.classList.add("active");
-        }
-        else{
-            reveal.classList.remove("active");
-        }
-    });
+		if (elemenTop < windowHeight - elementVisible) {
+			reveal.classList.add("active");
+		} else {
+			reveal.classList.remove("active");
+		}
+	});
 }
 
 window.addEventListener("scroll", reveal);
+
+$(document).ready(function () {
+	$(".trigger").on("click", function () {
+		$(".modal-wrapper").toggleClass("close");
+		return false;
+	});
+});
