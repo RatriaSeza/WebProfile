43b2cc5eba8ffbb910fb7e046b5884895f99c6f7
Satria <74123282+RatriaSeza@users.noreply.github.com>
2023-01-25 02:17:36 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -10,8 +10,6 @@ function responsive() {
 
 $(document).ready(function () {
 	$(window).scroll(function () {
-
-		console.log($(window).scrollTop());
 		if ($(window).scrollTop() > 100) {
 			$(".hero").addClass("blur");
 		} else {
